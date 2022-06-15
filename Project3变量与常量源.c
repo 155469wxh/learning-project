a23b8c2ae540c266d85994126fe1043bb525de81
bot155469 <107350585+155469wxh@users.noreply.github.com>
2022-06-15 15:49:43 +0800

    Add files via upload

---
+++ Project3变量与常量源.c
@@ -0,0 +1,17 @@
+
+
+
+#include<stdio.h>
+int main()
+{
+	int age = 20;
+	//int表示变量类型，age表示变量名称，=表示对变量进行赋值
+	double weight = 75.3;
+	age = age + 1;
+	weight = weight - 10;
+	printf("%d\n", age);
+	printf("%lf\n", weight);
+	//%d表示打印int，%f表示打印float，%lf表示打印double
+	return 0;
+}
+
