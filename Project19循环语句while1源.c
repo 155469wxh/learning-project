68cc9ce65bd1ae4a43c747cd2f85a76671572758
bot155469 <107350585+155469wxh@users.noreply.github.com>
2022-07-22 23:13:35 +0800

    Add files via upload

---
+++ Project19循环语句while1源.c
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+	int i = 0;
+	while (i <= 10)
+	{
+		printf("%d ", i);
+		i++;
+
+	}
+
+
+
+
+
+
+
+	return 0;
+}
