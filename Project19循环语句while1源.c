9e0c2f942c3869648ab10fe4ca194539e0e1d2de
bot155469 <107350585+155469wxh@users.noreply.github.com>
2022-07-24 21:19:03 +0800

    Add files via upload

--- Project19循环语句while1源.c
+++ Project19循环语句while1源.c
@@ -3,19 +3,27 @@
 #include <string.h>
 int main()
 {
-	int i = 0;
+	int i = 1;
 	while (i <= 10)
 	{
-		printf("%d ", i);
+		if (i==5)//千万注意=和==的区别
+			break;//在while循环中，break用于永久的终止循环
+		printf("%d \n", i);
+		i++;
+	}
+	while (i <= 10)
+	{
+		if (i == 5)
+			continue;//这个continue的意思是跳过continue以后的代码，直接跳出while，进行下一次判断是否要进入while循环
+		printf("%d \n", i);
 		i++;
 
+
 	}
-
-
-
-
-
-
-
+	/*int ch = getchar();
+	printf("%c\n", ch);
+	putchar(ch);
+	return 0;*/
 	return 0;
+	//就写到这吧，死循环罢了
 }
